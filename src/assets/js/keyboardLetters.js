e662d57cd90888f12a4667f055ee969663ec87c1
Will Cary <w.a.c.ary@gmail.com>
2022-02-02 09:11:00 -0500

    Implement 1.5 Flex Key in Keyboard

--- src/assets/js/keyboardLetters.js
+++ src/assets/js/keyboardLetters.js
@@ -2,4 +2,4 @@ export const LETTERS_FIRST_ROW = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', '
 
 export const LETTERS_SECOND_ROW = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
 
-export const LETTERS_THIRD_ROW = ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DELETE']
+export const LETTERS_THIRD_ROW = ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL']
