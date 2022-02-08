f4c63f10301eb79776cc15ee8872657308d7cad5
Will Cary <w.a.c.ary@gmail.com>
2022-02-08 09:13:19 -0500

    Implement Modal Open/Close Logic

--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -2,17 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "../css/helpmodal.css"
 
-function HelpModal({ showHelpModal }) {
-  return showHelpModal && <div className="help-modal">
+function HelpModal({ show, close }) {
+  return show && <div className="help-modal">
       <div className="help-modal__inner">
           Instructions here...
+          <button onClick={close}>X</button>
       </div>
           
   </div>;
 }
 
 HelpModal.propTypes = {
-    showHelpModal: PropTypes.bool.isRequired
+    show: PropTypes.bool.isRequired,
+    close: PropTypes.func.isRequired
 };
 
 export default HelpModal;
--- src/components/StatsModal.js
+++ src/components/StatsModal.js
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "../css/statsmodal.css"
 
-function StatsModal({ showStatsModal }) {
-  return showStatsModal && <div className="stats-modal">Stats here...</div>;
+function StatsModal({ show, close }) {
+  return show && <div className="stats-modal">
+    Stats here...
+    <button onClick={close}>X</button>
+  </div>;
 }
 
 StatsModal.propTypes = {
-    showStatsModal: PropTypes.bool.isRequired
+    show: PropTypes.bool.isRequired,
+    close: PropTypes.func.isRequired
 };
 
 export default StatsModal;
