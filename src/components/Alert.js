fa97dee6b31f45d1d0d804f13c07c2d96d1b1d1c
willcary <w.a.c.ary@gmail.com>
2022-03-07 14:34:20 -0500

    Update Alert with Congrats Message

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,10 +2,12 @@ import React, { useContext } from 'react';
 import { Context } from "../Context"
 
 export default function Alert() {
-  const { gameOver, isWord, solution } = useContext(Context)
+  const { gameOver, didWin, isWord, solution } = useContext(Context)
   
   if (!isWord) {
     return <div className="alert">Incorrect Spelling</div>
+  } else if (gameOver && didWin) {
+    return <div className="alert">Nice Job!</div>
   } else if (gameOver) {
     return <div className="alert">Solution: {solution}</div>
   } else {
